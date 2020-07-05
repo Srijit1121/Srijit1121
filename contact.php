e9a54fb078fbd0a9afd7c97d0ce3600ae239affb
Srijit koirala <67162503+Srijit1121@users.noreply.github.com>
2020-07-05 22:07:41 +0545

    sa

--- contact.php
+++ contact.php
@@ -1,13 +1,15 @@
 <?php
 $field_name = $_POST['cf_name'];
 $field_email = $_POST['cf_email'];
+$field_subject = $_POST['cf_subject'];
 $field_message = $_POST['cf_message'];
 
 $mail_to = 'ksrjit21@gmail.com';
-$subject = 'Message from a site visitor '.$field_name;
+$subject = 'cf_subject'.$field_name;
 
 $body_message = 'From: '.$field_name."\n";
 $body_message .= 'E-mail: '.$field_email."\n";
+$body_message .= 'Subject: '.$field_subject."\n";
 $body_message .= 'Message: '.$field_message;
 
 $headers = 'From: '.$field_email."\r\n";
